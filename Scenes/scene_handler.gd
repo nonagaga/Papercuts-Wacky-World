22122049d05fe268eecbf0906986f453a32090bd
drw_xd <drw2007@icloud.com>
2025-02-05 21:26:17 -0500

    I did a couple of things lol
    Added two new levels, a temp main menu UI, and a scenehandler scene(it does nothing rn but check its script)

---
+++ Scenes/scene_handler.gd
@@ -0,0 +1,3 @@
+extends Node
+#So I usually have a scene hander be the main scene and then place methods on it to keep track of all of the scenes being loaded/unloaded
+#so if you think youd be down to program that functionality in conjunction with the main menu/doors... go for it lol
